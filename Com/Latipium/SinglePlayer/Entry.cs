650da0b261ba99c1f2a391993168d7b3e2bdd8e6
Zach Deibert <zachdeibert@gmail.com>
2016-04-18 16:06:59 -0500

    Initial commit.

---
+++ Com/Latipium/SinglePlayer/Entry.cs
@@ -0,0 +1,22 @@
+﻿// Entry.cs
+//
+// Copyright (c) 2016 Zach Deibert.
+// All Rights Reserved.
+using System;
+using Com.Latipium.Security;
+
+namespace Com.Latipium.SinglePlayer {
+	/// <summary>
+	/// The main class for the server.
+	/// </summary>
+	public class Entry {
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		public static void Main(string[] args) {
+			LatipiumSecurity.Initialize("Com.Latipium.Modules.SinglePlayer", args.Length > 0 ? args[0] : null);
+		}
+	}
+}
+
